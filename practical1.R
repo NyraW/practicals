d87a05ccadf0168b24e9130b25f303bf82c9c45a
nyra williams <L00162304@student.lyit.ie>
2021-03-25 03:43:48 -0400

    Initial commit

--- practical1.R
+++ practical1.R
@@ -61,3 +61,19 @@ sorted_z
 seq3 <- seq(-100, 100, by=0.6)
 seq3
 sort(seq3, decreasing = TRUE)
+
+# Practical demo
+# add contents into vec1
+vec1 <- c("Hockey", "Football", "Baseball", "Curling", "Rugby", "Hurling", "Basketball", "Tennis", "Cricket", "Lacrosse")
+
+
+vec2 <- c(vec1, "Hockey", "Lacrosse", "Hockey", "Water Polo", "Hockey", "Lacrosse")
+
+vec2
+vec2_factor <- as.factor(vec2)
+vec2_factor
+
+# Extract the first, third, and sixth element
+# from vec2 and insert into vec3
+vec3 <- vec2[c(1,3,6)]
+vec3
